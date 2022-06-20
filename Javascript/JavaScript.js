54c03e5d8afb15facaa185b0af52d996daa7d870
jelandrewsosa <jelandrewsosa@gmail.com>
2022-06-20 20:27:51 +0800

    first commit

---
+++ Javascript/JavaScript.js
@@ -0,0 +1,14 @@
+﻿/* ================================= typing animation ================================= */
+var typed = new Typed(".typing",{
+    strings: [" ", "Web Designer","Web Developer","Graphic Designer","Computer Hardware Expert","Computer Technician","Business Owner","Technical Support Representative","Crypto Trader","NFT Collector","Youtuber","Musician","Gamer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var typed = new Typed(".attitude-set", {
+    strings: [" ", "Technical", "Excellent Problem Solver", "Creative", "Highly Motivated", "Fast Learner", "Passionate", "Pressure Handler", "Team Player", "Strategist", "Customer Oriented", "Computer Obsessed", "Attentive to Details", "Multi-Tasker", "Flexible", "Dependable", "Honest", "Energetic", "Hardworking"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
